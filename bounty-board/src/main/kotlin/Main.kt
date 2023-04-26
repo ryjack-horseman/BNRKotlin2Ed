14157f1f28c4f2755c9437e219039b67c1e69793
Ryan <rjackson@mobis-usa.com>
2023-04-26 11:56:29 -0400

    Finish The Unicorn's horn and the Magic Mirror challenge

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -5,10 +5,17 @@ fun main() {
     println(HERO_NAME)
 
     var hasSteed = false
+    val pubName = "The Unicorn's Horn"
+    var currentPublican = "John"
+    var currGold = 50
+    val drinkList: List<String> = listOf("mead", "wine", "LaCroix")
     var playerLevel= 4
     println(playerLevel)
 
     println("The hero embarks on her journey to locate the enchanted sword")
     playerLevel++
     println(playerLevel)
+
+    println("The hero discovers a magic mirror!")
+    println("The hero's new name is " + HERO_NAME.reversed())
 }
