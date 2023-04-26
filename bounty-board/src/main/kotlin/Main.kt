dc277561fe53bd28a93f00516ce3080a3492f6fd
Ryan <rjackson@mobis-usa.com>
2023-04-26 11:36:47 -0400

    Add val/var code, val is immutable/read-only -> think value, var is traditional mutable variable

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 fun main() {
-    println("Hello World!")
+    println("The hero announces her presence to the world")
+
+    val heroName = "Madrigal"
+    println(heroName)
+
     var playerLevel: Int = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword")
+    playerLevel++
     println(playerLevel)
 }
