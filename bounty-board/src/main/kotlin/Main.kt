a4ccf820444c8794f95150d64ef47ce66dfe9e05
Ryan <rjackson@mobis-usa.com>
2023-05-02 13:13:36 -0400

    Comment out challenges and add titles

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -4,18 +4,28 @@ fun main() {
     println("The hero announces her presence to the world")
     println(HERO_NAME)
 
-    var hasSteed = false
-    val pubName = "The Unicorn's Horn"
-    var currentPublican = "John"
-    var currGold = 50
-    val drinkList: List<String> = listOf("mead", "wine", "LaCroix")
+  //Ch2 hasSteed challenge
+//    var hasSteed = false
+    //Ch2 Unicorn's horn challenge
+//    val pubName = "The Unicorn's Horn"
+//    var currentPublican = "John"
+//    var currGold = 50
+//    val drinkList: List<String> = listOf("mead", "wine", "LaCroix")
     var playerLevel= 4
     println(playerLevel)
 
-    println("The hero embarks on her journey to locate the enchanted sword")
+    if(playerLevel == 1){
+        println("Meet Mr. Bubbles in the land of soft things")
+    } else {
+      println("Locate the enchanted sword.")
+    }
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
     playerLevel++
     println(playerLevel)
-
-    println("The hero discovers a magic mirror!")
-    println("The hero's new name is " + HERO_NAME.reversed())
+//  ch2 magic mirror challenge
+//    println("The hero discovers a magic mirror!")
+//    println("The hero's new name is " + HERO_NAME.reversed())
 }
