451fa5f4dacaa1abef59d5103befbea2bf4a6f1d
Ryan <rjackson@mobis-usa.com>
2023-05-04 14:06:39 -0400

    Add logic to hotswap the modifier function from callee and lambda function for changing the narrator's mood

--- NyetHack/src/main/kotlin/Narrator.kt
+++ NyetHack/src/main/kotlin/Narrator.kt
@@ -1,11 +1,48 @@
-val narrationModifier: (String) -> String = {
-    val numExclamationPoints = 3
-    it.uppercase() + "!".repeat(numExclamationPoints)
-}
+import java.lang.reflect.Modifier
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+var narrationModifier: (String) -> String = {it}
 
 fun narrate(
-    message: String
+    message: String,
+    modifier: (String) -> String = {narrationModifier(it)}
 ) {
 
-    println(narrationModifier(message))
+    println(modifier(message))
+}
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifer: (String) -> String
+    when(Random.nextInt(1..4)){
+        1 -> {
+            mood = "loud"
+            modifer = {message ->
+                val numExclamationPoints = 3
+                message.uppercase()+ "!".repeat(numExclamationPoints)
+            }
+        }
+        2 -> {
+            mood = "tired"
+            modifer = {message ->
+                message.lowercase().replace(" ", "... ")
+            }
+        }
+        3 -> {
+            mood = "unsure"
+            modifer = {message ->
+                "$message."
+            }
+        }
+        else -> {
+            mood = "professional"
+            modifer = {
+                message ->
+                "$message."
+            }
+        }
+    }
+    narrationModifier = modifer
+    narrate("The narrator begins to feel $mood")
 }
--- NyetHack/src/main/kotlin/NyetHack.kt
+++ NyetHack/src/main/kotlin/NyetHack.kt
@@ -1,4 +1,9 @@
 fun main(args: Array<String>) {
-    narrate("A hero enters the town of Kronstadt. What is their name?")
+    narrate("A hero enters the town of Kronstadt. What is their name?", { message ->
+        "\u001b[33;1m$message\u001b[0m"
+    })
     val heroName = readLine() ?: ""
+
+    changeNarratorMood()
+    narrate("$heroName heads to the town square.")
 }
