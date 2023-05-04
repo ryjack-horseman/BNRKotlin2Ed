eb4abe10ae899e74498c8ed8a8b1f404b77f14e0
Ryan <rjackson@mobis-usa.com>
2023-05-04 11:15:19 -0400

    Add null checks, introduction of the non-null assertion operator

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -4,12 +4,8 @@ var playerLevel = 0
 fun main() {
     println("$HERO_NAME announces her presence to the world")
     println("What level is $HERO_NAME?")
-    val playerLevelInput = readLine()!!
-    playerLevel = if(playerLevelInput.matches("""\d+""".toRegex())){
-        playerLevelInput.toInt()
-    }else{
-        1
-    }
+    playerLevel = readLine()?.toIntOrNull() ?: 0
+
     println("$HERO_NAME's level is $playerLevel")
 
   //Ch2 hasSteed challenge
@@ -38,7 +34,7 @@ private fun obtainQuest(
     playerClass: String = "paladin",
     hasBefriendedBarbarians: Boolean = true,
     hasAngeredBarbarians: Boolean = false
-): String = when(playerLevel) {
+): String? = when(playerLevel) {
         1 -> "Meet Mr. Bubbles in the land of soft things"
         in 2..5 -> {
             //try diplomacy
@@ -54,14 +50,18 @@ private fun obtainQuest(
         6 -> "Located the enchanted sword."
         7 -> "Recover the long-lost artifact of creation"
         8 -> "Defeat Nogartse, bringer of death and eater of worlds"
-        else -> "There are no quests right now"
+        else -> null
     }
 
 private fun readBountyBoard(){
-    println(
-        """
-        |$HERO_NAME approaches the bounty board. It reads:
-        |   "${obtainQuest(playerLevel).replace("Nogartse", "xxxxxxxx")}"
-        """.trimMargin()
-    )
+    val quest: String? = obtainQuest(playerLevel)
+    val message: String = quest?.replace("Nogartse", "xxxxxxxx")
+        ?.let { censoredQuest ->
+            """
+            |$HERO_NAME approaches the bounty board. It reads:
+            |   "$quest"
+            """.trimMargin()
+        } ?: "$HERO_NAME approaches the bountry board, but it is blank"
+    println(message)
 }
+// |   "${obtainQuest(playerLevel).replace("Nogartse", "xxxxxxxx")}"
