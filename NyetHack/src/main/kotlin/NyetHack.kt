4e057dc09b255109487fc18d1deb537be77f6514
Ryan <rjackson@mobis-usa.com>
2023-05-10 09:14:08 -0400

    Add trailing lambda syntax

--- NyetHack/src/main/kotlin/NyetHack.kt
+++ NyetHack/src/main/kotlin/NyetHack.kt
@@ -1,7 +1,7 @@
 fun main() {
-    narrate("A hero enters the town of Kronstadt. What is their name?", { message ->
+    narrate("A hero enters the town of Kronstadt. What is their name?"){ message ->
         "\u001b[33;1m$message\u001b[0m"
-    })
+    }
 
     val heroName = readLine()
     require(heroName != null && heroName.isNotEmpty()) {
