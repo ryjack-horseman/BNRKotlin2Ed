b789f794615efa2bb42c1f4fcc0bbd340e92f3f6
Ryan <rjackson@mobis-usa.com>
2023-05-10 11:12:37 -0400

    Changing a List's elements

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -4,7 +4,7 @@ private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
 
-    val patrons: List<String> = listOf("Eli", "Mordoc", "Sophie")
+    val patrons = mutableListOf("Eli", "Mordoc", "Sophie")
 
     val eliMessage = if(patrons.contains("Eli")){
         "$TAVERN_MASTER says: Eli's in the back playing cards"
@@ -19,4 +19,16 @@ fun visitTavern() {
         "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
     }
     println(othersMessage)
+
+    narrate("Eli leaves the tavern")
+    patrons.remove("Eli")
+    narrate("Alex enters the tavern")
+    patrons.add("Alex")
+
+    println(patrons)
+
+    narrate("Alex (VIP) enters the tavern")
+    patrons.add(0, "Alex")
+    patrons[0] = "Alexis"
+    println(patrons)
 }
