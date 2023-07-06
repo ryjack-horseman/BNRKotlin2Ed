e34ccb2ac1754fc47cd5d606933f21dcc6f99437
Ryan <rjackson@mobis-usa.com>
2023-07-06 11:14:20 -0400

    Add nested GameInput class to handle user input

--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -45,7 +45,17 @@ object Game {
             currentRoom.enterRoom()
 
             print("> Enter your command: ")
-            println("Last command: ${readLine()}")
+            GameInput(readLine()).processCommand()
+        }
+    }
+
+    private class GameInput(arg: String?){
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1) { "" }
+
+        fun processCommand() = when(command.lowercase()) {
+            else -> narrate("I'm not sure what you're trying to do")
         }
     }
 }
